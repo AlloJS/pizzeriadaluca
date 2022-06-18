060c1052bf5945185c2c48903de28df5b3b940a2
Angelo Bellanca <105851392+AlloJS@users.noreply.github.com>
2022-06-18 16:49:26 +0200

    Update scripts.js
    Ciao ti ho aggiunto il bordo rosso quando non si compila il form. Se ti può piacere

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+var inputName = document.getElementById('fname');
+var inputSurname = document.getElementById('lname');
+var inputTel = document.getElementById('cell');
+var inputAddress = document.getElementById('indirizzo');
+var inputPostalCode = document.getElementById('cap');
+var inputCity = document.getElementById('city');
+var inputSubmit = document.getElementById('input-submit');
+
+var form_convlid = [ inputName,
+                     inputSurname,
+                     inputTel,
+                     inputAddress,
+                     inputPostalCode,
+                     inputCity
+                    ];
+
 function getSelectOption() {
     var selectedValue = document.getElementById("select-qt").value;
     return selectedValue;
@@ -23,7 +39,21 @@ function deleteChild() {
     }
 }
 
-
 function addRows() {
     addSelectHTML();
 }
+
+function addClassAlert(){
+    for(i = 0; i < form_convlid.length; i++){
+        if(form_convlid[i].value === ""){
+            form_convlid[i].classList.add('red-border');
+        } else {
+            form_convlid[i].classList.remove('red-border');
+        }
+    };
+};
+                    
+window.addEventListener('change',addClassAlert);
+
+
+
